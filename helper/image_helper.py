c87a020653d7891f5e6ef4a49d856263eb6119db
Yusuff Jamal <yusuffjamal3@gmail.com>
2024-06-25 12:43:25 +0200

    image fix

--- helper/image_helper.py
+++ helper/image_helper.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from config import BATCH_SIZE
 
 
 def save_image_v2(images, caption, filename):
@@ -9,6 +10,10 @@ def save_image_v2(images, caption, filename):
     fig.suptitle(caption, fontsize=14, style='italic')
 
     for idx, image in enumerate(images):
+
+        if BATCH_SIZE > 1:
+            image = image[0]
+            
         # Plot the first image
         axes[idx].imshow(np.transpose(image.squeeze(), (1, 2, 0)))
         axes[idx].set_title(f"Image {idx + 1}")
